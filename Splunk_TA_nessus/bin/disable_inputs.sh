9f3a78c35f77f5be5d4340ca27ac7b79164d0a60
jefair2 <42381266+jefair2@users.noreply.github.com>
2018-08-14 10:33:17 -0400

    Add files via upload

---
+++ Splunk_TA_nessus/bin/disable_inputs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+found=false
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [[ $line = "[HQDA-IPN]" ]]; then
+        found=true
+    fi
+    if [[ $line = "[Reporting server]" ]]; then
+        found=true
+    fi
+    #if [[ $line = "[HQDA]" ]]; then
+    #    found=true
+    #fi
+
+    if [[ "$found" = true ]] ; then
+        if [[ $line = "disabled"* ]]; then
+            line=$(echo $line | sed 's/0/1/')
+            found=false
+        fi
+    fi
+    echo $line
+done < "/opt/splunk/etc/apps/Splunk_TA_nessus/local/tenable_sc_inputs.conf" > "/opt/splunk/etc/apps/Splunk_TA_nessus/local/tenable_sc_inputs_2.conf"
+mv /opt/splunk/etc/apps/Splunk_TA_nessus/local/tenable_sc_inputs_2.conf /opt/splunk/etc/apps/Splunk_TA_nessus/local/tenable_sc_inputs.conf
+chown splunk:splunk /opt/splunk/etc/apps/Splunk_TA_nessus/local/tenable_sc_inputs.conf
+
